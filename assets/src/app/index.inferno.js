0d2b578f9d60c835b0ea9ccac5fb6c2c8e987692
元彦 <yuanyan.cao@alibaba-inc.com>
2017-03-16 13:52:17 +0800

    rename

---
+++ assets/src/app/index.inferno.js
@@ -0,0 +1,25 @@
+
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+import List from './list/index.inferno';
+import Banner from './banner/index.inferno';
+
+export default class App extends Component {
+
+    componentDidMount() {
+        // console.log('inferno render in client');
+    }
+
+    render() {
+
+        const props = this.props || {};
+
+        return (
+            <div>
+                <Banner data={props.bannerData} />
+                <List data={props.listData} />
+            </div>
+        );
+
+    }
+};
